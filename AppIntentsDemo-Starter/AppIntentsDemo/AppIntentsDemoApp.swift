68d3f7179f118ba85e760968af38681d6abe9cb7
Ashli Rankin <ashlirankin18@gmail.com>
2023-11-13 11:34:03 -0500

    Added files
    Signed-off-by: Ashli Rankin <ashlirankin18@gmail.com>

---
+++ AppIntentsDemo-Starter/AppIntentsDemo/AppIntentsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentsDemoApp.swift
+//  AppIntentsDemo
+//
+//  Created by Ashli Rankin on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntentsDemo-Starter/AppIntentsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIntentsDemo
+//
+//  Created by Ashli Rankin on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
